21d62109dc8afa93c3d15bce0cdc2816bce2fee6
malw <anycad@qq.com>
2025-03-05 11:24:37 +0800

    镜像矩阵导致的法线反向

---
+++ AnyCAD.Basic/Analysis/Analysis_Solid.cs
@@ -0,0 +1,25 @@
+﻿using AnyCAD.Foundation;
+using System;
+
+namespace AnyCAD.Demo.Geometry
+{
+    class Analysis_Solid : TestCase
+    {
+        public override void Run(IRenderView renderer)
+        {
+            var cyl = ShapeBuilder.MakeCylinder(new GAx2(), 10, 20, 0);
+
+            var node = renderer.ShowShape(cyl, ColorTable.Gray);
+
+            SolidExplor se = new SolidExplor();
+            se.Initialize(cyl);
+
+            var faceIds = se.GetSharedFaceIDsByIndex(2);
+            foreach(var id in faceIds)
+            {
+                node.SetFaceColor(id, ColorTable.HotPink);
+            }
+        }
+    }
+ 
+}
